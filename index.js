fb87adaaf0a99d7baa877194263c8440dc081077
brocksm <brocks.mike@gmail.com>
2019-12-13 21:27:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ function toQuaternion([x, y, z]) {
     	cZ = Math.cos(z);
     	
     return [
-		cX * cY * cZ + sX * sY * sZ,
-		sX * cY * cZ - cX * sY * sZ,
-		cX * sY * cZ + sX * cY * sZ,
-		cX * cY * sZ - sX * sY * cZ
+	cX * cY * cZ + sX * sY * sZ,
+	sX * cY * cZ - cX * sY * sZ,
+	cX * sY * cZ + sX * cY * sZ,
+	cX * cY * sZ - sX * sY * cZ
     ];
   }
   
 function toDegrees([a, b, c, d]) {
     return [
-		Math.atan2(2 * (a * b + c * d), 1 - 2 * (b * b + c * c)) * 180 / Math.PI,
-		Math.asin(Math.max(-1, Math.min(1, 2 * (a * c - d * b)))) * 180 / Math.PI,
-		Math.atan2(2 * (a * d + b * c), 1 - 2 * (c * c + d * d)) * 180 / Math.PI
+	Math.atan2(2 * (a * b + c * d), 1 - 2 * (b * b + c * c)) * 180 / Math.PI,
+	Math.asin(Math.max(-1, Math.min(1, 2 * (a * c - d * b)))) * 180 / Math.PI,
+	Math.atan2(2 * (a * d + b * c), 1 - 2 * (c * c + d * d)) * 180 / Math.PI
     ];
   }
   
@@ -34,21 +34,21 @@ function slerp(q0, q1, t) {
     if (d < 0) {q1.map(v => v *= -1;)}
 	
     if (d > 0.9995) {
-		q1.map((v, i) => {v -= q0[i];})
+	q1.map((v, i) => {v -= q0[i];})
 		
-		var vα = [
-			q0[0] + q1[0] * α,
-			q0[1] + q1[1] * α,
-			q0[2] + q1[2] * α,
-			q0[3] + q1[3] * α
-		],
+	var vα = [
+		q0[0] + q1[0] * α,
+		q0[1] + q1[1] * α,
+		q0[2] + q1[2] * α,
+		q0[3] + q1[3] * α
+	],
 		
-		f = Math.hypot(vα);
+	f = Math.hypot(vα);
 		
-		vα = va.map(v => v / f);
+	vα = va.map(v => v / f);
 		
-		return vα;
-	}
+	return vα;
+    }
 	
     var θ_ = Math.acos(Math.max(-1, Math.min(1, d)));
 	
@@ -59,32 +59,32 @@ function slerp(q0, q1, t) {
     q1.map(v => v / p);
 	
     var θ = θ_ * α,
-		s = Math.sin(θ);
-		c = Math.cos(θ);
+	s = Math.sin(θ);
+	c = Math.cos(θ);
 		
     return [
-		q0[0] * c + q1[0] * s,
-		q0[1] * c + q1[1] * s,
-		q0[2] * c + q1[2] * s,
-		q0[3] * c + q1[3] * s	
-	];
+	q0[0] * c + q1[0] * s,
+	q0[1] * c + q1[1] * s,
+	q0[2] * c + q1[2] * s,
+	q0[3] * c + q1[3] * s	
+    ];
 }
   
 function dot(a, b) {
     var x;
 	
     for (var i = 0; i < a.length; i++) {
-		x += a[i] * b[i];
-	}
+	x += a[i] * b[i];
+    }
 	
     return x;
 }
 
 function quaternionProduct(q0, q1) {
     return [
-		q0[0] * q1[0] - q0[1] * q1[1] - q0[2] * q1[2] - q0[3] * q1[3],
+	q0[0] * q1[0] - q0[1] * q1[1] - q0[2] * q1[2] - q0[3] * q1[3],
     	q0[0] * q1[1] + q0[1] * q1[0] + q0[2] * q1[3] - q0[3] * q1[2],
     	q0[0] * q1[2] - q0[1] * q1[3] + q0[2] * q1[0] + q0[3] * q1[1],
     	q0[0] * q1[3] + q0[1] * q1[2] - q0[2] * q1[1] + q0[3] * q1[0]		
-	]
+    ]
 }
