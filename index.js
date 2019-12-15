d1a7d1d2eb90f286b52092479bd5b1b0374fbe6a
brocksm <brocks.mike@gmail.com>
2019-12-14 22:06:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@
 	return x;
     }
 
-    function quaternionProduct(q0, q1) {
+    function product(q0, q1) {
 	return [
 	    q0[0] * q1[0] - q0[1] * q1[1] - q0[2] * q1[2] - q0[3] * q1[3],
     	    q0[0] * q1[1] + q0[1] * q1[0] + q0[2] * q1[3] - q0[3] * q1[2],
